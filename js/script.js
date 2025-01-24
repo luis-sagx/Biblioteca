b6583fa2cd1abf6b27c2badc942c29d5c03b4a39
Teo344 <gabriel2004mat@gmail.com>
2025-01-24 12:20:21 -0500

    Notifiaciones

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-class Libro{
-    constructor(id, titulo, autor, genero){
+class Libro {
+    constructor(id, titulo, autor, genero) {
         this.id = id;
         this.titulo = titulo;
         this.autor = autor;
@@ -7,14 +7,24 @@ class Libro{
     }
 }
 
-const libro1 = new Libro(1, "El Principito", "Antoine de Saint-Exupéry", "Ficcion" );
-const libro2 = new Libro(2, "Don Quijote de la Mancha", "Miguel de Cervantes", "Novela" );
-const libro3 = new Libro(3, "Cien años de soledad", "Gabriel García Márquez", "Realismo magico" );
+const libro1 = new Libro(1, "El Principito", "Antoine de Saint-Exupéry", "Ficcion");
+const libro2 = new Libro(2, "Don Quijote de la Mancha", "Miguel de Cervantes", "Novela");
+const libro3 = new Libro(3, "Cien años de soledad", "Gabriel García Márquez", "Realismo magico");
 
 const librosDisponibles = [
-   libro1,
-   libro2,
-   libro3
+    libro1,
+    libro2,
+    libro3
 ];
 
 const librosPrestados = [];
+
+
+function mostrarNotificacion(mensaje) {
+    const notificacion = document.getElementById("notificaciones");
+    notificacion.textContent = mensaje;
+
+    setTimeout(() => {
+        notificacion.textContent = "";
+    }, 10000);
+}
