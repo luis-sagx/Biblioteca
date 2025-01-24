095c4d87da491effa4a62f73da4c4f54eae3435b
Luis Eduardo Sagnay Pilamunga <sagnayluis99@gmail.com>
2025-01-24 11:30:05 -0500

    arreglo de libros

---
+++ js/script.js
@@ -0,0 +1,20 @@
+class Libro{
+    constructor(id, titulo, autor, genero){
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+    }
+}
+
+const libro1 = new Libro(1, "El Principito", "Antoine de Saint-Exupéry", "Ficcion" );
+const libro2 = new Libro(2, "Don Quijote de la Mancha", "Miguel de Cervantes", "Novela" );
+const libro3 = new Libro(3, "Cien años de soledad", "Gabriel García Márquez", "Realismo magico" );
+
+const librosDisponibles = [
+   libro1,
+   libro2,
+   libro3
+];
+
+const librosPrestados = [];
